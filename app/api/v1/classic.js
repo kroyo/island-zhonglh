977448c0681153806234a34d255209413afe5996
kroyo <kroyoshi@163.com>
2019-12-25 23:04:30 +0800

    旧岛小样node+koa学习 4-8完成

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,11 +1,27 @@
 const Router = require('koa-router')
 const router = new Router()
 
+const { HttpException, ParameterException } = require('../../../core/http-exception')
+
 router.post('/v1/classic/latest', (ctx, next) => {
   ctx.body = { key: 'classic' }
+  const path = ctx.params
+  const query = ctx.request.query
+  const headers = ctx.request.header
+  const body = ctx.request.body
+
+  if(true){
+    // 动态  面向对象方式  一个类
+    // const error = new HttpException('为什么错误', 10001, 400)
+    const error = new ParameterException()
+    throw error
+  }
+  ctx.body = {
+    key: 'classic'
+  }
+  // throw new Error('API Exception')
   // 监听异常
   // 输出一段有意义的提示
-  throw new Error('API Exception')
 })
 
 module.exports = router
--- middlewares/exception.js
+++ middlewares/exception.js
@@ -1,11 +1,22 @@
+const { HttpException } = require('../core/http-exception')
+
 const catchError = async (ctx, next) => {
   try {
     await next();
   } catch (error) {
+    if(error instanceof HttpException) {
+      ctx.body = {
+        msg: error.msg,
+        error_code: error.errorCode,
+        request: `${ctx.method} ${ctx.path}`
+      }
+      ctx.status = error.code
+    }
+
     // error 堆栈调用信息
     // error 简化 清晰明了的信息  给前端
     // HTTP Status Code 2xx, 4xx, 5xx
-    ctx.body = '服务器有点问题，你等一下。';
+    // ctx.body = '服务器有点问题，你等一下。';
     // message
     // error_code 详细， 开发者自己定义  10001 20003
 
