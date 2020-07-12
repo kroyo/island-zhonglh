ebe0d72c0adff97e57f48381cdb2c16691807bca
kroyo <kroyoshi@163.com>
2020-07-12 16:41:31 +0800

    移除code-master

--- code-master/js-base/async-demo.js
+++
@@ -1,6 +0,0 @@
-// 异步
-console.log(100);
-setTimeout(() => {
-  console.log(300)
-},1000)
-console.log(200)
--- code-master/webpack-demo/src/fn.js
+++
@@ -1,9 +0,0 @@
-function fn() {
-  console.log('fn')
-}
-const name = 'b'
-
-export {  // 注意这里不能有 default ！！！
-  fn,
-  name
-}
