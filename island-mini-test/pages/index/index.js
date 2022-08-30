b17a958fe8b0718d1b89d063b2fe00602ae023dc
zhonglh <kroyoshi@163.com>
2022-08-30 14:38:51 +0800

    feat: 添加小程序测试demo

--- island-mini-test/pages/index/index.js
+++ island-mini-test/pages/index/index.js
@@ -133,6 +133,35 @@ Page({
       }
     })
   },
+  onAdd() {
+    wx.request({
+      url: 'http://localhost:3000/v1/book/add',
+      method: 'POST',
+      data:{
+        "author": '["[美]保罗·格雷厄姆"]',
+        "binding": "平装",
+        "category": "编程",
+        "id": 7,
+        "image": "https://img3.doubanio.com/lpic/s4669554.jpg",
+        "images": '{"large": "https://img3.doubanio.com/lpic/s4669554.jpg"}',
+        "isbn": "9787115249494",
+        "pages": "264",
+        "price": "49.00元",
+        "pubdate": "2011-4",
+        "publisher": "人民邮电出版社",
+        "subtitle": "硅谷创业之父Paul Graham文集",
+        "summary": "本书是硅谷创业之父Paul Graham 的文集，主要介绍黑客即优秀程序员的爱好和动机，讨论黑客成长、黑客对世界的贡献以及编程语言和黑客工作方法等所有对计算机时代感兴趣的人的一些话题。书中的内容不但有助于了解计算机编程的本质、互联网行业的规则，还会帮助读者了解我们这个时代，迫使读者独立思考。\\n本书适合所有程序员和互联网创业者，也适合一切对计算机行业感兴趣的读者。",
+        "title": "黑客与画家",
+        "translator": '["阮一峰"]'
+      },
+      success: res => {
+        console.log(res.data)
+      },
+      header: {
+        Authorization: this._encode()
+      }
+    })
+  },
   // 取消点赞
   onDisLike() {
     wx.request({
@@ -166,7 +195,7 @@ Page({
 
   onGetBookDetail(){
     wx.request({
-      url: 'http://localhost:3000/v1/book/1120/detail',
+      url: 'http://localhost:3000/v1/book/7/detail',
       method: 'GET',
       success: res => {
         console.log(res.data)
@@ -240,7 +269,7 @@ Page({
       url: 'http://localhost:3000/v1/book/add/short_comment',
       method: 'POST',
       data: {
-        content:'春风十里不如有你春风十里不如有你',
+        content:'春风十里不如有你',
         book_id:1120
       },
       // like key%
